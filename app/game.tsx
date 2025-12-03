f2c5783f00bd81287202c9cbd6e1d7bd2120d366
seongyu <a92875664@gmail.com>
2025-12-03 22:01:54 +0900

    안드로이드 상단바 숨김 설정

--- app/game.tsx
+++ app/game.tsx
@@ -1,10 +1,16 @@
 import GameScreen from "../src/screens/GameScreen";
 import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
 
 export default function Game() {
   return (
     <>
-      <StatusBar hidden={true} />
+      <StatusBar
+        hidden={true}
+        style="light"
+        translucent={true}
+        backgroundColor="transparent"
+      />
       <GameScreen />
     </>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import StartScreen from "../src/screens/StartScreen";
 import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
 
 export default function Index() {
   return (
     <>
-      <StatusBar hidden={true} />
+      <StatusBar
+        hidden={true}
+        style="light"
+        translucent={true}
+        backgroundColor="transparent"
+      />
       <StartScreen />
     </>
   );
